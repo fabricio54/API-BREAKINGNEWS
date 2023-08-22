cee5eb0d4f5995a66430e4c55425bac3db472eba
Fabricio Alves <fabricioallves2020@gmail.com>
2023-08-22 10:15:26 -0300

    UPDATE: atualização feita na importação do json

--- src/routes/swagger.route.cjs
+++ src/routes/swagger.route.cjs
@@ -1,3 +1,4 @@
+/*
 import { Router } from "express";
 const router = Router();
 
@@ -8,3 +9,15 @@ router.use("/", swaggerUi.serve);
 router.get("/", swaggerUi.setup(swaggerDocument));
 
 export default router;
+*/
+
+const router = require("express").Router();
+
+const swaggerUi = require("swagger-ui-express");
+
+const swaggerDocument = require("../swagger.json");
+
+router.use("/", swaggerUi.serve);
+router.use("/", swaggerUi.setup(swaggerDocument));
+
+module.exports = router;
